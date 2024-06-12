89644ec5b30bb0eaf50f0b6672535720be1f76db
biplabmon <biplab8629@gmail.com>
2024-06-12 08:39:58 +0530

    complete work exprience section using moving border component from aceternity ui library

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Clients from "@/components/Clients";
+import Exprience from "@/components/Exprience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -15,6 +16,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Exprience />
       </div>
     </main>
   );
